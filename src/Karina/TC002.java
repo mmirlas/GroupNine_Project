bebed4223307fd409ea68c459dfd81a5dcec4167
Karina <kportillo0828@gmail.com>
2020-11-12 12:57:32 -0500

    created new class

---
+++ src/Karina/TC002.java
@@ -0,0 +1,4 @@
+package Karina;
+
+public class TC002 {
+}
--- src/Karina/Task001.java
+++ src/Karina/Task001.java
@@ -3,6 +3,7 @@ package Karina;
 public class Task001 {
     public static void main(String[] args) {
         System.out.println("Hello");
+
     }
 
 }
